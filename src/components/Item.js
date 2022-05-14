f1383c8cfa5cdb9d433ed5a01cf60c2f8f0da93e
Justin Jeffries <justincyjeffries@gmail.com>
2022-05-14 00:22:28 -0600

    commit tree c5c9212ea1f67075723308c5c3f7ac79255de687

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [appClass, setAppClass] = useState(true)
+  function handleClick(){
+    setAppClass((appClass)=>!appClass)
+  }
   return (
-    <li className="">
+    <li className={appClass ? " " : "in-cart"}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">Add to Cart</button>
     </li>
   );
 }
